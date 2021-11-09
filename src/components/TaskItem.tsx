e7f59c50f13c6d4d8bd5b1b3176af8d311aa8a52
Yoshi <takatantakatan@gmail.com>
2021-11-09 22:22:25 +0900

    2021/11/9

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -3,16 +3,28 @@ import { Task } from "../Types";
 
 type Props = {
   task: Task;
+  // returnは無いのでvoidで指定
+  handleDone: (task: Task) => void;
+  handleDelete: (task: Task) => void;
 };
-const TaskItem: React.FC<Props> = ({ task }) => {
+const TaskItem: React.FC<Props> = ({ task, handleDone, handleDelete }) => {
   return (
     // リスト表示するのでliタグを使用
-    <li>
+    <li className={task.done ? "done" : ""}>
       <label>
-        <input type="checkbox" className="checkbox-input" />
+        <input
+          type="checkbox"
+          className="checkbox-input"
+          // handleDoneは引数にtaskを渡す必要があるので関数の形にする
+          onClick={() => handleDone(task)}
+          // 初期値の設定
+          defaultChecked={task.done}
+        />
         <span className="checkbox-label">{task.title}</span>
       </label>
-      <button className="btn is-delete">削除</button>
+      <button onClick={() => handleDelete(task)} className="btn is-delete">
+        削除
+      </button>
     </li>
   );
 };
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import TaskItem from "./TaskItem";
+import { Task } from "../Types";
+
+type Props = {
+  tasks: Task[];
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+};
+const TaskList: React.FC<Props> = ({ tasks, setTasks }) => {
+  const handleDone = (task: Task) => {
+    setTasks((prev) =>
+      prev.map((t) => (t.id === task.id ? { ...task, done: !task.done } : t))
+    );
+  };
+
+  const handleDelete = (task: Task) => {
+    setTasks((prev) => prev.filter((t) => t.id !== task.id));
+  };
+
+  return (
+    <div className="inner">
+      {tasks.length <= 0 ? (
+        "登録されたTODOはまだありません。"
+      ) : (
+        <ul className="task-list">
+          {/* 一覧表示するにはmapで表示 */}
+          {tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              handleDone={handleDone}
+              handleDelete={handleDelete}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default TaskList;
