2182e6990df2d86c750726708f943933da864e08
Yoshi <takatantakatan@gmail.com>
2021-11-09 15:53:42 +0900

    3rd

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Task } from "../Types";
+
+type Props = {
+  task: Task;
+};
+
+function App:<Props> =({task}) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
